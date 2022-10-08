fb18e7331549b3a2a81063849831dbaaadb952b0
Ras Haile <105021283+Rashaileguevara@users.noreply.github.com>
2022-10-08 22:05:28 +0300

    Create conversion_problem.py

---
+++ conversion_problem.py
@@ -0,0 +1,8 @@
+#problem : receives miles and convert it to kilometers
+#kilometer = mile * 1.60938
+#enters 5 miles 
+#5 miles equals to 8.04 kilometers
+miles = input ("Enter the miles ")
+miles = int(miles)
+kilometer = miles * 1.60938
+print("{} miles equals to {} kilometer".format(miles, kilometer))
