ffb8f3f8d52b4332f8d7b245f17a2d32ad4ea70c
Ras Haile <105021283+Rashaileguevara@users.noreply.github.com>
2022-10-08 22:04:38 +0300

    Create looping.py

---
+++ looping.py
@@ -0,0 +1,12 @@
+#first step
+'''
+for i in [2, 4, 6, 8, 10]:
+    print("i = ", i)
+# second step   
+for i in range (10):
+    print("i = ", i)
+#if i%2 == 0 even
+'''
+for i in range (1,21):
+  if ((i%2) != 0):
+    print("i = ", i)
